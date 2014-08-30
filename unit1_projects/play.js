99ff20a3fc145b5d193b3c4de14e1aea0c4de108
Miscelaineous <misselainefinn@gmail.com>
2014-08-30 13:00:35 -0700

    moved Unit 1

---
+++ unit1_projects/play.js
@@ -0,0 +1,49 @@
+/*var myNum = 5;
+myNum + 5;
+typeof 3;
+typeof 9.2;
+typeof myNum;
+
+console.log(typeof myNum);*/
+
+/*
+
+confirm("I am ready to play");
+
+var age = prompt("What's your age?");
+var userAnswer = prompt("Do you want to race Bieber on stage?");
+var feedback = prompt("Please rate game out of 10");
+
+if(age < 13) {
+    console.log("we do not take any responsibility for your play");
+}
+else    {
+    console.log("Enjoy!");
+}
+
+    console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+
+    console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
+if(userAnswer === "yes")    {
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!")
+}
+else {
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'")
+}
+
+if(feedback > 8)    {
+    console.log("Thank you! We should race at the next concert!")
+}
+else    {
+    console.log("I'll keep practicing coding and racing.")
+}
+
+*/
+
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+console.log("Happy New Year!!");
+
+
